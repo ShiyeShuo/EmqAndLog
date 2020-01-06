66ad86e06a60887329b20c3799fc3030f6db6918
ls <425871815@qq.com>
2020-01-06 23:17:52 +0800

    emq

--- emqtest/src/main/java/com/ls/emqtest/control/TestController.java
+++ emqtest/src/main/java/com/ls/emqtest/control/TestController.java
@@ -1,19 +1,23 @@
 package com.ls.emqtest.control;
 
 import com.ls.emqtest.client.MqttPushClient;
+import com.ls.emqtest.entity.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("emqTest")
+@RequestMapping("/emqTest")
 public class TestController
 {
-    @PostMapping("/sendToEmq")
-    public void sendtoEmq(String top,String msg)
+    @Autowired
+    MqttPushClient mqttPushClient;
+    @RequestMapping("/sendToEmq")
+    public void sendtoEmq(Message data)
     {
-        //MqttPushClient.publish(top,msg);
+        mqttPushClient.publish(data.getTopic(),data.getMsg());
 
     }
 }
