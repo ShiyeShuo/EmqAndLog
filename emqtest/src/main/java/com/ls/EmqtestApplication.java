7624b714816232174912fcb956616efb677ca5fc
ls <425871815@qq.com>
2020-01-12 23:04:07 +0800

    操作日志

--- emqtest/src/main/java/com/ls/EmqtestApplication.java
+++ emqtest/src/main/java/com/ls/EmqtestApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = {"com.ls.emqtest"})
-@ComponentScan(basePackages = {"com.ls.client"})
+@SpringBootApplication
+//@ComponentScan(basePackages = {"com.ls.client"})
 public class EmqtestApplication {
 
     public static void main(String[] args) {
